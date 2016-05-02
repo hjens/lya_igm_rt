cf9714dbd50bf6b7e1013c6f0ed340a695a53aa1
Hannes <hannesj1@gmail.com>
2016-05-02 17:20:13 +0200

    added process_output file

---
+++ process_output.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def process_output(transmissions_file, line_model, params_dict):
+    """
+    Process the transmission functions in the given output
+    file and calculate the transmitted fraction given the
+    supplied line model
+
+    :param transmissions_file: The filename containing the transmissions
+    :param line_model: Callable that gives the line flux as a function
+    of wavelength in Angstrom
+    :param params_dict: Dictionary containing parameters
+    :return: Array with the transmitted fractions
+    """
+    pass
